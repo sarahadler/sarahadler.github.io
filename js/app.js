5dfe117490b6b7409624ff47ac26e730e876078d
sarah@spoonuniversity.com <sadler@u.northwestern.edu>
2014-02-19 18:21:26 -0500

    added slow scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+function scrollToInformation(event) {
+    event.preventDefault();
+    var diff = $(document).height() - $(window).height();
+    var dest = ($(this.hash).offset().top > diff) ? diff : $(this.hash).offset().top;
+    if (dest <= 480) { dest = 0; }
+        $('html, body').animate({scrollTop: dest}, 800, 'swing');
+}
+
 $(document).ready(function() {
 	$(".fancybox-thumb").fancybox({
 		prevEffect	: 'none',
@@ -12,45 +20,7 @@ $(document).ready(function() {
 			}
 		}
 	});
-});
 
-(function($, window, undefined) {
-    function scrollingSlider(selector, beginpos, endpos) {
-        var slide,
-            timeout = 0;
-        slide = function () {
-            var scrollpos;
-            window.clearTimeout(timeout);
-            timeout = 0,
-                scrollpos = $(window).scrollTop();
-            $(selector).each(function () {
-                var $this = $(this),
-                    margintarget,
-                    margin = parseInt($this.css('margin-left'), 10),
-                    step;
-                if (scrollpos <= beginpos) {
-                    margintarget = -1000;
-                } else if (scrollpos >= endpos) {
-                    margintarget = 0;
-                } else {
-                    margintarget = Math.round(-1000 * (endpos - scrollpos) / (endpos - beginpos));
-                }
-                if (margintarget === margin) {
-                    return; // nothing more to do
-                }
-                step = margintarget - margin;
-                if (Math.abs(step) > 25) {
-                    step = 25 * (step < 0 ? -1 : 1);
-                }
-                $this.css('margin-left', margin + step);
-                if (!timeout && (margin + step) !== margintarget) {
-                    timeout = window.setTimeout(slide, 10);
-                }
-            });
-        };
-        $(window).on('scroll', slide);
-    }
-    $(document).ready(function (){
-        scrollingSlider('.post-129, .post-169', 100, 300);
-    });
-}(jQuery, window));
+    $('.quicklinks a').on('click', scrollToInformation);
+    
+});
